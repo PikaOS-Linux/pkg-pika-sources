b6a9f51b58fa64316fadd735e92da472bc6c18d0
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-23 00:11:50 +0300

    march model

---
+++ mainv3.sh
@@ -0,0 +1,23 @@
+export DEBIAN_FRONTEND="noninteractive"
+export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_BUILD_OPTIONS="nocheck notest terse"
+export DPKG_GENSYMBOLS_CHECK_LEVEL=0
+
+# Clone Upstream
+cd ./pika-sources
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+LOGNAME=root dh_make --createorig -y -l -p pika-sources_7.1 || echo "dh-make didn't go clean"
+dpkg-buildpackage --no-sign
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
