04a5dc2acdb07b6da9ed4cedbc648cdb69502fad
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-24 00:15:02 +0300

    fix main

--- mainv3.sh
+++ mainv3.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+VERSION="7.2"
+
 export DEBIAN_FRONTEND="noninteractive"
 export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
 export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
@@ -18,7 +20,7 @@ cd ./pika-sources
 apt-get build-dep ./ -y
 
 # Build package
-LOGNAME=root dh_make --createorig -y -l -p pika-sources_7.2 || echo "dh-make didn't go clean"
+LOGNAME=root dh_make --createorig -y -l -p pika-sources_"$VERSION" || echo "dh-make: Ignoring Last Error"
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
